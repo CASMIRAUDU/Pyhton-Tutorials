015eb1d8f801e41219607f1ab3e4c2f7dbb6b32a
CASMIRAUDU <casmirbandekaji@gmail.com>
2020-07-23 14:03:12 +0100

    Initial Project Version

---
+++ variables.py
@@ -0,0 +1,20 @@
+x = 1
+y = 2.5
+name = 'John'
+is_cool = True
+#Assignment; give 5 examples of variable names and assign values to them,of different types.
+w = 4
+d = 5.9
+name = 'Casmir'
+a_player = False
+actors = ('obi', 'pawpaw', 'falz')
+fav_colors =('white','grey','blue')
+'''
+This is
+a python class
+where we will be learning how to code
+'''
+print(y)
+print(is_cool)
+print(x)
+print(fav_colors)
